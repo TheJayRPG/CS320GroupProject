1ab31577c9dc69280c770a9818d01e5b6f783108
Dennis Balan <43692805+dennisbalan@users.noreply.github.com>
2022-04-25 19:59:23 -0700

    Add files via upload

---
+++ CS320GroupProject/create_file_name.py
@@ -0,0 +1,16 @@
+def create_file_name(shape, occurence):
+    if(shape == 3):
+        filename = "triangle" + "_" + str(occurence)
+        return filename
+    elif(shape == 4):
+        filename = "square" + "_" + str(occurence)
+        return filename
+    elif(shape == 5):
+        filename = "pentagon" + "_" + str(occurence)
+        return filename
+    elif(shape == 6):
+        filename = "hexagon" + "_" + str(occurence)
+        return filename
+    else:
+        filename = "file_" + str(occurence)
+
