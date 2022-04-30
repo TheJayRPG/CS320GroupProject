d5740f925046f64d43c6f6de514ecba58d343768
Alex <97657354+UltimaAngel@users.noreply.github.com>
2022-04-29 20:05:02 -0700

    Added string return methods
    Added methods to each class that returns string versions of each class and added improved comments.

--- Alex-ThoughtProcess/cellTP.py
+++ Alex-ThoughtProcess/cellTP.py
@@ -1,70 +1,146 @@
 """
 Alexander Hong
 CS 320
-Cell Thought Process
+https://github.com/TheJayRPG/CS320GroupProject
+Custom Game of Life - Cell Thought Process
+cellTP.py
 
-This file is used to track the changes made by a selected cell.
-
+This file provides the classes used to store information on a selected cell's
+changes and the observations it makes to surrounding cells.
 To be used from main.py via import.
+For independent testing, import into cellTPView.py
 """
-import main
+
+"""
+TPCell
+
+Class that stores the relevant data of a selected cell.
+    xPos        Integer representing the y position of the cell.
+    yPos        Integer representing the y position of the cell.
+    age         Integer representing the current lifecycle the cell is on.
+    status      Integer representing if a cell if dead(0) or alive(1).
+    nearby      Integer representing the number of surrounding cells.
+    liveNear    Integer representing the number of nearby living cells.
+    deadNear    Integer representing the number of nearby dead cells.
+    history     History of the 10 most recent history Logs for the cell.
+"""
+class TPCell():
+    def __init__(self, x, y, a, s, n, ln, dn, h):
+        self.xPos = x
+        self.yPos = y
+        self.age = a
+        self.status = s
+        self.nearby = n
+    	self.liveNear = ln
+    	self.deadNear = dn
+        self.history = h
+
+    """
+    strInfo
+
+    Returns the information in the cell in a string format.
+    """
+    def strInfo(self):
+        cellInfo = ""
+        cellInfo += "[" + str(self.xPos) + ", " + str(self.yPos) + "]\n"
+        cellInfo += "  Age: " + str(self.age) + "\n"
+        cellInfo += "  Nearby: " + str(self.nearby) + "\n"
+        cellInfo += "  Living: " + str(self.liveNear) + "\n"
+        cellInfo += "  Dead: " + str(self.deadNear) + "\n"
+        statDict = {0: "Dead", 1: "Alive"}
+        cellInfo += "  Status: " + statDict.get(self.status, "Other"))
+        return cellInfo
+
+    """
+    historySize
+
+    Returns the size of the history object stored.
+    """
+    def historySize(self):
+        if self.history != None:
+            return self.history.size
+
+    """
+    historyInfo
+
+    Returns the history object's info as a string.
+    """
+    def historyInfo(self):
+        return self.history.strInfo()
 
 """
 Log
 
-Class that stores the information on the changes to be made for a cell.
+Class that stores the information on the changes to be made for a TPCell.
 Performs like a linked list, refering to previous entries for the cell.
-
-printLog
-Displays the information held in a log for testing purposes.
+    age         Integer representing the current lifecycle the cell is on.
+    status      Integer representing the number of surrounding cells.
+    nearby      Integer representing the number of surrounding cells.
+    liveNear    Integer representing the number of nearby living cells.
+    deadNear    Integer representing the number of nearby dead cells.
+    change      Indicates if this log's status is different from the previous
 """
 class Log():
     def __init__(self, cell):
-        self.nearby = cell.neighbors
-        self.living = cell.living
-        self.dead = cell.dead
-        self.gen = cell.generation
-        self.state = ""
+        self.age = cell.age
+        self.status = cell.status
+        self.nearby = cell.nearby
+    	self.liveNear = cell.liveNear
+    	self.deadNear = cell.deadNear
         self.prev = None
         self.change = False
 
-    def changeCheck(self):
-        pass
+    """
+    changeCheck
 
-    def printLog(self):
-        print("Nearby: " + str(self.nearby))
-        print("Living: " + str(self.living))
-        print("Dead: " + str(self.dead))
-        print("Gen: " + str(self.gen))
-        if self.prev != None:
-            print("Prev: Exists")
-        else:
-            print("Prev: None")
-        print("Change: " + str(self.change))
+    Checks if the previous log had a different state (alive or dead) and
+    toggles the change variable to True.
+    """
+    def changeCheck(self):
+        if self.prev != None and self.prev.status != self.status:
+            self.change = True
+
+    """
+    strInfo
+
+    Provides a simplified version of the log info in string format.
+    """
+    def strInfo():
+        logInfo = ""
+        logInfo += "[" + str(cell.xPos) + ", " + str(cell.yPos) + "]\n"
+        logInfo += "  Age: " + str(self.age) + "\n"
+        logInfo += "  Nearby: " + str(self.nearby) + "\n"
+        logInfo += "  Living: " + str(self.liveNear) + "\n"
+        logInfo += "  Dead: " + str(self.deadNear) + "\n"
+        statDict = {0: "Dead", 1: "Alive"}
+        logInfo += "  Status: " + statDict.get(self.status, "Other"))
+        return logInfo
 
 """
 History
 
 Class used for recording the throught process history of a cell using logs.
 Holds the 10 most recent logs of a cell.
-
-updateRecord
-Recreates the history record by iterating through the 10 most recent logs.
-
-updateRecent
-Tells the history object to update itself with the provided new log.
-
-getString
-Returns a string interpretation of the history object.
-
-printHistory
-Displays the log info held in the history object for testing purposes.
 """
 class History():
     def __init__(self, recentLog):
+        self.size = 1
         self.recent = recentLog
         self.record = [recentLog]
 
+    """
+    getSize
+
+    Returns the size of the History object.
+    """
+    def getSize(self):
+        return self.size
+
+    """
+    updateRecord
+
+    Recreates the history record by iterating through the 10 most recent logs.
+    """
     def updateRecord():
         log = self.recent
         newRecord = []
@@ -76,48 +152,40 @@ class History():
                 log = log.prev
         self.record = newRecord
 
+    """
+    updateRecent
+
+    Tells the history object to update itself with the provided new log.
+    """
     def updateRecent(self, newLog):
         newLog.prev = self.recent
         self.recent = newLog
+        if self.size < 10:
+            self.size += 1
         updateRecord()
 
-    def getString():
-        strVer = ""
-        for a in range(10):
-            strVer += "[" + str(self.record[a].gen) + "] "
-            strVer += "L: " + str(self.record[a].living) + " "
-            strVer += "D: " + str(self.record[a].dead) + " "
-            strVer += self.record[a].state
-            if (self.record[a].chane == True):
-                strVer += " CHANGE"
-        return strVer
+    """
+    strInfo
 
-    def printHistory():
-        for a in range(10):
-            pass
+    Provides a string version of each log in the record array.
+    """
+    def strInfo():
+        hisInfo = ""
+        for log in record:
+            hisInfo += log.strInfo()
+            hisInfo += "\n"
+        return hisInfo
 
 """
 recordCell
 
 Given the x and y positions of an array of cells, begins recording
 logs of the cell's history.
-Returns a version of the cell with a history object set to self.past
+Returns a version of the cell with a history object set to self.history.
 """
-def recordCell(xPos, yPos, cellArray):
-    cell = cellArray[yPos][xPos]
+def recordCell(x, y, cellArray):
+    cell = cellArray[y][x]
     cellLog = Log(cell)
-    cell.past = History(cellLog)
+    cell.history = History(cellLog)
 
     return cell
-
-"""
-main
-
-Used for testing the classes and methods in cellTP.py independently.
-Currently unimplemented.
-"""
-def main():
-    pass
-
-if __name__=='__main__':
-    main()
